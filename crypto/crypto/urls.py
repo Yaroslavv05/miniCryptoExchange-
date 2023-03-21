7e7716dc0629eba44ffa58d7bd9b01263f49604f
MRX580 <andrejchenko0511@mail.ru>
2023-03-21 10:20:45 +0100

    add some update in architecture

--- crypto/crypto/urls.py
+++ crypto/crypto/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('login', include('registered.urls')),
+    path('login/', include('registered.urls')),
+    path('', include('main.urls')),
 ]
