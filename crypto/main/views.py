26b57bf0b4ba8545ea4ab517b78cad26570aabe8
MRX580 <andrejchenko0511@mail.ru>
2023-03-23 21:02:38 +0100

    add chart

--- crypto/main/views.py
+++ crypto/main/views.py
@@ -2,36 +2,42 @@ from django.shortcuts import render
 import binance
 from django.http import JsonResponse
 
+client = binance.Client()
 
 def index(request):
-    client = binance.Client()
     # print(client.get_all_tickers())
     # print(client.get_symbol_info('XRPUSDT')['baseAsset'])
     # print(client.get_symbol_info('XRPUSDT')['priceChangePercent'])
     # print(client.get_ticker())
+    data = {
+        'BTC': client.get_ticker(symbol='BTCUSDT'),
+        'ETH': client.get_ticker(symbol='ETHUSDT'),
+        'BNB': client.get_ticker(symbol='BNBUSDT'),
+        # 'USDT': client.get_ticker(symbol='USDT'),
+        'ADA': client.get_ticker(symbol='ADAUSDT'),
+        'XRP': client.get_ticker(symbol='XRPUSDT'),
+        'DOT': client.get_ticker(symbol='DOTUSDT'),
+        'UNI': client.get_ticker(symbol='UNIUSDT'),
+        'MATIC': client.get_ticker(symbol='MATICUSDT'),
+        'DOGE': client.get_ticker(symbol='DOGEUSDT'),
+            }
+    # print(client.get_ticker(symbol='BTCUSDT'))
+    return render(request, 'main/index.html', {'data': data})
+
+
+def chart(request):
     data_price = [i[1] for i in client.get_klines(symbol='BTCUSDT', interval='1m')][:100]
     data = {'data': data_price}
     min_data = int(min(data['data']).split('.')[0])
     max_data = int(max(data['data']).split('.')[0])
     data_len = []
     for i in reversed(range(len(data_price))):
-        data_len.append(str(i+1) + 'm ago')
+        data_len.append(str(i + 1) + 'm ago')
     data = {'min_data': min_data, 'max_data': max_data, 'data': data['data'][:100], 'data_len': data_len}
-    # data = {
-    #     'BTC': client.get_symbol_info('BTCUSDT'),
-    #     'ETH': client.get_symbol_info('ETHUSDT'),
-    #     'BNB': client.get_symbol_info('BNBUSDT'),
-    #     'USDT': client.get_symbol_info('USDT'),
-    #     'ADA': client.get_symbol_info('ADAUSDT'),
-    #     'XRP': client.get_symbol_info('XRPUSDT'),
-    #     'DOT': client.get_symbol_info('DOTUSDT'),
-    #     'UNI': client.get_symbol_info('UNIUSDT'),
-    #     'MATIC': client.get_symbol_info('MATICUSDT'),
-    #     'DOGE': client.get_symbol_info('DOGEUSDT'),
-    #         }
     if is_ajax(request=request):
         return JsonResponse(data, status=200)
-    return render(request, 'main/index.html')
+    return render(request, 'main/chart.html')
+
 
 def is_ajax(request):
     return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
