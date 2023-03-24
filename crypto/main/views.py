57f1adfa1110cfbb3bb010bc312d5399a27fd52f
MRX580 <andrejchenko0511@mail.ru>
2023-03-24 16:47:33 +0100

    remove func chart from main

--- crypto/main/views.py
+++ crypto/main/views.py
@@ -25,19 +25,5 @@ def index(request):
     return render(request, 'main/index.html', {'data': data})
 
 
-def chart(request):
-    data_price = [i[1] for i in client.get_klines(symbol='BTCUSDT', interval='1m')][:100]
-    data = {'data': data_price}
-    min_data = int(min(data['data']).split('.')[0])
-    max_data = int(max(data['data']).split('.')[0])
-    data_len = []
-    for i in reversed(range(len(data_price))):
-        data_len.append(str(i + 1) + 'm ago')
-    data = {'min_data': min_data, 'max_data': max_data, 'data': data['data'][:100], 'data_len': data_len}
-    if is_ajax(request=request):
-        return JsonResponse(data, status=200)
-    return render(request, 'main/chart.html')
-
-
 def is_ajax(request):
     return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
