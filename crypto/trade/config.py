df4a7dffe29a1fe1acb720fb9760c554d41975f4
chorn <chornyyaroslav5@gmail.com>
2023-04-07 00:09:27 +0300

    add full file config

--- crypto/trade/config.py
+++ crypto/trade/config.py
@@ -2,5 +2,130 @@ INFO = {
     'ETH': {
         'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
         'api': 'https://mainnet.infura.io/v3/e5f7798e7a404fb19bb38e66d9e615d9'
-    }
+    },
+    'BNB': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://bsc-dataseed.binance.org/'
+    },
+    'XRP': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://bsc-dataseed.binance.org/'
+    },
+    'ADA': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://bsc-dataseed.binance.org/'
+    },
+    'DOGE': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://bsc-dataseed.binance.org/'
+    },
+    'MATIC': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://mainnet.infura.io/v3/e5f7798e7a404fb19bb38e66d9e615d9'
+    },
+    'DOT': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://bsc-dataseed.binance.org/'
+    },
+    'LTC': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://bsc-dataseed.binance.org/'
+    },
+    'SHIB': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://mainnet.infura.io/v3/e5f7798e7a404fb19bb38e66d9e615d9'
+    },
+    'TRX': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://bsc-dataseed.binance.org/'
+    },
+    'AVAX': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://avalanche-mainnet.infura.io/v3/e5f7798e7a404fb19bb38e66d9e615d9'
+    },
+    'DAI': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://mainnet.infura.io/v3/e5f7798e7a404fb19bb38e66d9e615d9'
+    },
+    'LINK': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://mainnet.infura.io/v3/e5f7798e7a404fb19bb38e66d9e615d9'
+    },
+    'UNI': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://mainnet.infura.io/v3/e5f7798e7a404fb19bb38e66d9e615d9'
+    },
+    'ATOM': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://bsc-dataseed.binance.org/'
+    },
+    'LEO': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://mainnet.infura.io/v3/e5f7798e7a404fb19bb38e66d9e615d9'
+    },
+    'TON': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://mainnet.infura.io/v3/e5f7798e7a404fb19bb38e66d9e615d9'
+    },
+    'OKB': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://mainnet.infura.io/v3/e5f7798e7a404fb19bb38e66d9e615d9'
+    },
+    'FIL': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://bsc-dataseed.binance.org/'
+    },
+    'LDO': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://mainnet.infura.io/v3/e5f7798e7a404fb19bb38e66d9e615d9'
+    },
+    'CRO': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://mainnet.infura.io/v3/e5f7798e7a404fb19bb38e66d9e615d9'
+    },
+    'NEAR': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://bsc-dataseed.binance.org/'
+    },
+    'APE': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://mainnet.infura.io/v3/e5f7798e7a404fb19bb38e66d9e615d9'
+    },
+    'ARB': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://mainnet.infura.io/v3/e5f7798e7a404fb19bb38e66d9e615d9'
+    },
+    'QNT': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://mainnet.infura.io/v3/e5f7798e7a404fb19bb38e66d9e615d9'
+    },
+    'EOS': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://bsc-dataseed.binance.org/'
+    },
+    'GRT': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://mainnet.infura.io/v3/e5f7798e7a404fb19bb38e66d9e615d9'
+    },
+    'FTM': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://mainnet.infura.io/v3/e5f7798e7a404fb19bb38e66d9e615d9'
+    },
+    'AAVE': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://mainnet.infura.io/v3/e5f7798e7a404fb19bb38e66d9e615d9'
+    },
+    'MANA': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://mainnet.infura.io/v3/e5f7798e7a404fb19bb38e66d9e615d9'
+    },
+    'BIT': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://mainnet.infura.io/v3/e5f7798e7a404fb19bb38e66d9e615d9'
+    },
+    'XTZ': {
+        'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+        'api': 'https://bsc-dataseed.binance.org/'
+    },
+
 }
