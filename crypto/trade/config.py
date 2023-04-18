a541e66ef5c4b9a492d5a95cc6de3a217bd70c4f
Yarik <chornyyaroslav5@gmail.com>
2023-04-18 16:36:01 +0300

    add edit

--- crypto/trade/config.py
+++ crypto/trade/config.py
@@ -1,4 +1,9 @@
+# сайт https://www.infura.io
 INFO = {
+    # 'BTC': {
+    #     'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
+    #     'api': 'https://mainnet.infura.io/v3/e5f7798e7a404fb19bb38e66d9e615d9' #ETH
+    # },
     'ETH': {
         'wallet': '0xA52a8e0D2671e53CA993A04F29De16fC0194b13a',
         'api': 'https://mainnet.infura.io/v3/e5f7798e7a404fb19bb38e66d9e615d9' #ETH
