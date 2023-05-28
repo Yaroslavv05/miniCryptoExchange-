14123414c18de2a5001e69127cdeb6e574fb7c78
MRX580 <andrejchenko0511@mail.ru>
2023-05-28 22:17:01 +0300

    Update

--- crypto/main/views.py
+++ crypto/main/views.py
@@ -1,29 +1,63 @@
 from django.shortcuts import render
 import binance
+from django.http import JsonResponse
 
 client = binance.Client()
 
 
 def index(request):
-    symbols = ['BTC', 'ETH', 'BNB', 'ADA', 'XRP', 'DOT', 'UNI', 'MATIC', 'DOGE']
-    data = {}
-
-    for symbol in symbols:
-        ticker = client.get_ticker(symbol=f'{symbol}USDT')
-        price = round(float(ticker['lastPrice']) * 1.03, 4)
-        price_change_percent = round(float(ticker['priceChangePercent']) + 2.5, 2)
-        high_price = round(float(ticker['highPrice']), 4)
-        low_price = round(float(ticker['lowPrice']), 4)
-        volume = float(ticker['volume'])
-
-        data[symbol] = {
-            'price': price,
-            'per': price_change_percent,
-            'highPrice': high_price,
-            'lowPrice': low_price,
-            'volume': volume
-        }
-
+    # print(client.get_all_tickers())
+    # print(client.get_symbol_info('XRPUSDT')['baseAsset'])
+    # print(client.get_symbol_info('XRPUSDT')['priceChangePercent'])
+    data = {
+        'BTC': {'price': round(float(client.get_ticker(symbol='BTCUSDT')['lastPrice']) * 1.03, 4),
+                'per': round(float(client.get_ticker(symbol='BTCUSDT')['priceChangePercent']) + 2.5, 2),
+                'highPrice': round(float(client.get_ticker(symbol='BTCUSDT')['highPrice']), 4),
+                'lowPrice': round(float(client.get_ticker(symbol='BTCUSDT')['lowPrice']), 4),
+                'volume': float(client.get_ticker(symbol='BTCUSDT')['volume'])},
+        'ETH': {'price': round(float(client.get_ticker(symbol='ETHUSDT')['lastPrice']) * 1.03, 4),
+                'per': round(float(client.get_ticker(symbol='ETHUSDT')['priceChangePercent']) + 2.5, 2),
+                'highPrice': round(float(client.get_ticker(symbol='ETHUSDT')['highPrice']), 4),
+                'lowPrice': round(float(client.get_ticker(symbol='ETHUSDT')['lowPrice']), 4),
+                'volume': float(client.get_ticker(symbol='ETHUSDT')['volume'])},
+        'BNB': {'price': round(float(client.get_ticker(symbol='BNBUSDT')['lastPrice']) * 1.03, 4),
+                'per': round(float(client.get_ticker(symbol='BNBUSDT')['priceChangePercent']) + 2.5, 2),
+                'highPrice': round(float(client.get_ticker(symbol='BNBUSDT')['highPrice']), 4),
+                'lowPrice': round(float(client.get_ticker(symbol='BNBUSDT')['lowPrice']), 4),
+                'volume': float(client.get_ticker(symbol='BNBUSDT')['volume'])},
+        'ADA': {'price': round(float(client.get_ticker(symbol='ADAUSDT')['lastPrice']) * 1.03, 4),
+                'per': round(float(client.get_ticker(symbol='ADAUSDT')['priceChangePercent']) + 2.5, 2),
+                'highPrice': round(float(client.get_ticker(symbol='ADAUSDT')['highPrice']), 4),
+                'lowPrice': round(float(client.get_ticker(symbol='ADAUSDT')['lowPrice']), 4),
+                'volume': float(client.get_ticker(symbol='ADAUSDT')['volume'])},
+        'XRP': {'price': round(float(client.get_ticker(symbol='XRPUSDT')['lastPrice']) * 1.03, 4),
+                'per': round(float(client.get_ticker(symbol='XRPUSDT')['priceChangePercent']) + 2.5, 2),
+                'highPrice': round(float(client.get_ticker(symbol='XRPUSDT')['highPrice']), 4),
+                'lowPrice': round(float(client.get_ticker(symbol='XRPUSDT')['lowPrice']), 4),
+                'volume': float(client.get_ticker(symbol='XRPUSDT')['volume'])},
+        'DOT': {'price': round(float(client.get_ticker(symbol='DOTUSDT')['lastPrice']) * 1.03, 4),
+                'per': round(float(client.get_ticker(symbol='DOTUSDT')['priceChangePercent']) + 2.5, 2),
+                'highPrice': round(float(client.get_ticker(symbol='DOTUSDT')['highPrice']), 4),
+                'lowPrice': round(float(client.get_ticker(symbol='DOTUSDT')['lowPrice']), 4),
+                'volume': float(client.get_ticker(symbol='DOTUSDT')['volume'])},
+        'UNI': {'price': round(float(client.get_ticker(symbol='UNIUSDT')['lastPrice']) * 1.03, 4),
+                'per': round(float(client.get_ticker(symbol='UNIUSDT')['priceChangePercent']) + 2.5, 2),
+                'highPrice': round(float(client.get_ticker(symbol='UNIUSDT')['highPrice']), 4),
+                'lowPrice': round(float(client.get_ticker(symbol='UNIUSDT')['lowPrice']), 4),
+                'volume': float(client.get_ticker(symbol='UNIUSDT')['volume'])},
+        'MATIC': {'price': round(float(client.get_ticker(symbol='MATICUSDT')['lastPrice']) * 1.03, 4),
+                  'per': round(float(client.get_ticker(symbol='MATICUSDT')['priceChangePercent']) + 2.5, 2),
+                  'highPrice': round(float(client.get_ticker(symbol='MATICUSDT')['highPrice']), 4),
+                    'lowPrice': round(float(client.get_ticker(symbol='MATICUSDT')['lowPrice']), 4),
+                    'volume': float(client.get_ticker(symbol='MATICUSDT')['volume'])},
+        'DOGE': {'price': round(float(client.get_ticker(symbol='DOGEUSDT')['lastPrice']) * 1.03, 4),
+                 'per': round(float(client.get_ticker(symbol='DOGEUSDT')['priceChangePercent']) + 2.5, 2),
+                 'highPrice': round(float(client.get_ticker(symbol='DOGEUSDT')['highPrice']), 4),
+                'lowPrice': round(float(client.get_ticker(symbol='DOGEUSDT')['lowPrice']), 4),
+                'volume': float(client.get_ticker(symbol='DOGEUSDT')['volume'])},
+    }
+    if is_ajax(request=request):
+        return JsonResponse(data, status=200)
     return render(request, 'main/Bitfinex.html', {'data': data})
 
 
